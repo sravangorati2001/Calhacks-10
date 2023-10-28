382936ec712b37bf18670c57c089754630455fb4
Harshita B <honeyblue2604@gmail.com>
2023-10-28 15:33:08 -0700

    Setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useQuery } from 'convex/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { api } from "../convex/_generated/api";
 import './App.css'
 
 function App() {
   const tasks = useQuery(api.tasks.get);
   return (
-    <div className="App">
-      {tasks?.map(({_id, text}) => (
-        <div key={_id}>{text}</div>
-      ))}
-    </div>
+    // Add the page components here as created
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={} />
+        <Route path="/home" element={}>
+          <Route path="/home/isbn:" element={} />
+        </Route>
+        <Route path="/user" element={}>
+          <Route path="/user/settings" element={} />
+          <Route path="/user/listing" element={} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
